832db5279cc713b8f83efb82c265823350af4732
Sudhanshu Kumar <96191536+SudhanshuKCsit@users.noreply.github.com>
2022-05-12 19:01:19 +0000

    p3.c complete

---
+++ pps-test1/p3.c
@@ -0,0 +1,29 @@
+# include <stdio.h>
+
+int input()
+{
+  int a;
+  printf("enter the value \n");
+  scanf("%d ",&a);
+  return a; 
+  }
+  int add(int a,int b)
+{
+  int sum;
+  sum=a+b;
+  return sum;
+}
+void output(int a,int b,int sum)
+{
+  printf("sum of %d + %d is %d\n",a,b,sum);
+}
+int main()
+{
+  
+  int a,b,sum;
+  a=input();
+  b=input();
+  sum=add(a,b);
+  output(a,b,sum);
+  return 0;
+}
