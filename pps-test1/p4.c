a9a93b119ef37d61eacc8c9b9b2db3cf488008ef
Sudhanshu Kumar <96191536+SudhanshuKCsit@users.noreply.github.com>
2022-05-14 18:50:44 +0000

    p4 .c completed program

---
+++ pps-test1/p4.c
@@ -0,0 +1,27 @@
+# include <stdio.h>
+
+void input(int *a,int *b)
+{
+  int d;
+  printf("Enter the value for a,b\n");
+  scanf("%d %d",a,b);
+}
+void add(int a,int b,int *sum)
+{
+  scanf("%n",sum);
+    
+}
+void output(int a,int b,int sum)
+{
+  sum=a+b;
+  printf("The sum of two numbers is %d\n",sum);
+}
+int main()
+{
+  int a,b,sum;
+  input(&a,&b);
+  add(a,b,&sum);
+  output(a,b,sum);
+  return 0;
+
+}
